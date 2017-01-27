739d8d3ffcdf5379d04949c85ae5e908107d513a
j-extine <j-extine@onu.edu>
2017-01-26 23:30:26 -0500

    Add files via upload

---
+++ HWv2/Simple.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="HelloWorldStyle.css"/>
+        <title>Hello World</title>
+    </head>
+    <body>
+        <a href="Fancy.php" ID="button">Fancy</a>
+        <h1>Hello World!</h1>
+    </body>
+</html>
