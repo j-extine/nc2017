3b7c5c34f5c51ad0c9dff084aaacfd55a8b0be21
j-extine <j-extine@onu.edu>
2017-03-29 16:14:41 -0400

    Add files via upload

---
+++ CSS_Skills/discography.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="Play-with-CSS.css"/>
+        <title>Deliverance Discography</title>
+    </head>
+    <body>
+        <ul class="navBar">
+            <li><a href = discography.php>Discography</a></li>
+            <li><a href = index.php>Home</a></li>
+        </ul>
+        <img id="logo" src="Deliverance_Logo.jpg">
+        <div>
+            <h>Discography</h>
+            <p></p>
+           
+            <div class="album">
+                <p>Deliverance</p>
+                <p>(1989)</p>
+                <img src="Deliverance-Deliverance.jpg">
+            </div>
+            
+            <div class="album">
+                <p>Weapons of Our Warfare</p>
+                <p>(1990)</p>
+                <img src="Deliverance-Weapons_of_Our_Warfare.jpg">
+            </div>
+            
+            <div class="album">
+                <p>What a Joke</p>
+                <p>(1991)</p>
+                <img src="Deliverance-What_A_Joke.jpeg">
+            </div>
+            
+            <div class="album">
+                <p>Stay of Execution</p>
+                <p>(1992)</p>
+                <img src="Deliverance-Stay_of_Execution.jpg">
+            </div>
+            
+            <div class="album">
+                <p>Learn</p>
+                <p>(1993)</p>
+                <img src="Deliverance-Learn.jpg">
+            </div>
+            
+            <div class="album">
+                <p>River Disturbance</p>
+                <p>(1994)</p>
+                <img src="Deliverance-River_Disturbance.jpg">
+            </div>
+            
+            <div class="album">
+                <p>Camelot in Smithereens</p>
+                <p>(1995)</p>
+                <img src="Deliverance-Camelot_in_Smithereens.jpg">
+            </div>
+            
+            <div class="album">
+                <p>Assimilation</p>
+                <p>(2001)</p>
+                <img src="Deliverance-Assimilation.jpg">
+            </div>
+            
+            <div class="album">
+                <p>As Above - So Below</p>
+                <p>(2007)</p>
+                <img src="Deliverance-As_Above_So_Below.jpg">
+            </div>
+            
+            <div class="album">
+                <p>Hear What I say</p>
+                <p>(2013)</p>
+                <img src="Deliverance-Hear_What_I_Say.jpg">
+            </div>
+        </div> 
+    </body>
+</html>
---
+++ CSS_Skills/index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="Play-with-CSS.css"/>
+        <title>Deliverance Band</title>
+    </head>
+    <body>
+        <ul class="navBar">
+            <li><a href = discography.php>Discography</a></li>
+            <li><a href = index.php>Home</a></li>
+        </ul>
+        <img id="logo" src="Deliverance_Logo.jpg">
+       <div>
+            <h>Deliverance Home</h>
+            <p>Deliverance is an American Christian thrash metal band which 
+                later shifted more towards heavy metal and progressive metal. 
+                Founded by Jimmy P. Brown II in 1985, many members have either 
+                stayed in the lineup for a number of years, or left and returned
+                to the lineup. Jimmy P. Brown II has been the only constant 
+                member of Deliverance.</p>
+            <p>Deliverance formed in 1985 as a speed metal outfit, where the 
+               Christian metal community first encountered them on the 
+               compilation album California Metal. They put forth their 
+               self-titled debut album Deliverance in 1989 with 
+               Jimmy P. Brown II (guitars and lead vocals), Glenn Rogers (lead guitar), 
+               Brian Khairullah (bass), and Chris Hyde (drums). In 1989, 
+               Deliverance's track "If You Will" from their self-titled album 
+               appeared on Hot Metal Summer II along with a recorded interview.</p>
+            <p>Deliverance garnered significant attention in the Christian metal 
+               community with their 1990 release Weapons of Our Warfare. During
+               this time, the band replaced guitarist Glenn Rogers with George Ochoa.
+               Deliverance released a video on MTV for the title track of Weapons. 
+               The track was also included on Intense Records compilation album
+               Hot Metal Summer III Hot Licks - Cold Facts.</p>
+            <p>During the production of their third album, What a Joke, 
+               Brian Khairullah and Chris Hyde were replaced with Mike Grato 
+               and Kevin Lee respectively. The album did not perform as well, 
+               and tensions within the band resulted in the departure of George Ochoa.</p>
+            <p>Their fourth album was a major change in direction for the band musically.
+               Stay of Execution went in a more progressive metal direction, 
+               complete with new guitarist Mike Phillips.</p>
+            <p>In an interview with Doug Van Pelt of HM Magazine, when asked 
+                about the musical change, Jimmy P. Brown explained:</p>
+            <p>"I got tired of being labeled 'Oh, there's Deliverance.... 
+                the Metallica clones are back.' You know, that got really old. 
+                Because in a sense we were kind of ripping them off. 
+                But I definitely know that the Lord wants us to be original. 
+                I want to be original. So I think it was time."
+                — HM Video Magazine</p>
+            <p>While this move did anger fans at the time, others feel that Stay 
+               represents Deliverance's best work. Their fifth album, Learn, 
+               continued in a progressive direction with new bassist Manny Morales
+               and lead guitarist Jonathan Maddux. From this time on, the other 
+               positions in the band changed with increasing frequency, but the 
+               core of the band remained Jimmy Brown and Manny Morales.</p>
+            <p>Deliverance released River Disturbance in 1994 and 
+               Camelot in Smithereens in 1995. Camelot was the poorest seller in
+               the Deliverance catalog at the time.</p>
+            <p>After a four-year hiatus, Deliverance resurfaced in 2000 and 
+               began recording their next studio album, Assimilation, which was 
+               released in the following year. Assimilation represented another 
+               change in style with a shift towards a more electronic/industrial
+               sound. Deliverance again retreated until 2006.</p>
+            <p>In 2006, Deliverance reformed with a lineup of Jimmy P. Brown II,
+               Mike Phillips, Corin Jae Scott, Tim Kronyak, and Mike Reed. 
+               The band's next album As Above - So Below was released on 
+               April 24, 2007. Full length songs from the album can currently be
+               heard on the band's official MySpace page. The album featured a
+               mix of all their past material (musically) with some more modern 
+               metal elements.</p>
+            <p>Deliverance announced they would be releasing a new album sometime 
+               in 2010 called The Annals of Subterfuge. This would mark 
+               Deliverance's 25th Anniversary. Though rumors persist of reuniting 
+               with past members, it became apparent that Jimmy would be recording 
+               the upcoming album with Mike Phillips.</p>
+            <p>Though the group officially disbanded in 1996, there have been fan 
+               demands for concert performances and new albums which caused the 
+               intermittent reunions every 4–5 years. Jimmy P. Brown II has spent 
+               the past 10 years working in the casino industry in Las Vegas, but 
+               currently resides in Alabama continuing his career with the casino industry.
+               Jimmy has been quoted as saying: "I moved to Alabama to be close 
+               with my family, and to try regain normalcy for my family. Having 
+               lived in the fantasy land of Las Vegas for the past 10 years, I wanted
+               to stay in the casino biz, but didn't want my kids growing up the Vegas way".</p>
+            <p>In 2010, Deliverance played the only show without Jimmy P. Brown II at NordicFest.</p>
+            <p>On May 20, 2011 Mike Phillips announced that Deliverance was finally 
+                coming to end and would be performing their final show in August 2011.
+                However, by July 2012, Deliverance had resurfaced again and been
+                working on their tenth studio album Hear What I Say!, which was 
+                released on September 3, 2013 on Roxx Records and would be their final album. 
+                However, Jimmy Brown recently has stated that Hear What I Say! 
+                will most likely not be their final album.</p>
+            <p>Though Brown stated that Hear What I Say! would be the last album, 
+                he announced that Deliverance has new material to release in 2016. 
+                The band, whose lineup consists of former guitarist George Ochoa,
+                former drummer Jim Chaffin and bassist Victor Máciás, played Exodo
+                Fest in Mexico with Silent Planet and Grave Robber.</p>
+            <p>(Source: Wikipedia)</p>
+            <img id="band" src="Deliverance.jpg">
+        </div> 
+    </body>
+</html>
