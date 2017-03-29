2c5262751a2cf17dbba7bb2c23d79b884255559e
j-extine <j-extine@onu.edu>
2017-03-29 16:16:11 -0400

    Add files via upload

---
+++ Ad/HTML/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type=text/css href="../CSS/AdCSS.css"/>
+        <title>Theocracy Ghost Ship Advertisement</title>
+    </head>
+    <body>
+        <div id="cover"></div>
+        <div id="fog">
+            <div id="textBox">
+                <p class="text" id="line1">THE NEW ALBUM</p>
+                <p class="text" id="line2">GHOST SHIP</p>
+                <p class="text" id="line3">OUT WORLDWIDE ON</p>
+                <p class="text" id="line4"><span id="O">O</span>CTOBER 28<span id="th">TH</span></p>
+            </div>
+        </div>
+    </body>
+</html>
