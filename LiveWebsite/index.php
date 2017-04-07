fa9a7fd9a1a822a97a9183cec24130a39d7aab4d
j-extine <j-extine@onu.edu>
2017-04-07 17:42:24 -0400

    Add files via upload

---
+++ LiveWebsite/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel='stylesheet' type='text/css' href='liveCSS.css'>
+        <title>Jarod Extine's Site Navigator</title>
+    </head>
+    <body>
+        
+        <ul id='navbar'>
+            <li><a href="HW/index.php">Hello World</a></li>
+            <li><a href="HWv2/Simple.php">Innovative Hello World</a></li>
+            <li><a href="CSS_Skills/index.php">Play with CSS</a></li>
+            <li><a href="Ad/HTML/index.php">Mimic an Ad</a></li>
+            <li><a href="PHP_Calendar/index.php">PHP 1</a></li>
+            <li><a href="JavaScript-Play/index.php">JavaScript Play</a></li>
+            <li><a href="index.php">Home</a></li>
+        </ul>
+        
+        <div id='content'>
+            <h>Jarod Extine's Site Navigator</h>
+        
+            <p>Hello, welcome to my site navigator. The links at the top will 
+               take you to each of the sites that I have made thus far. 
+               Also if you wish to read the sites accompanying paper the 
+               links are down below. Thanks for stopping by.</p>
+        </div>
+        
+        <div id="pdfContainer">
+            <ul id="pdfLinks">
+                <li><a href="PDFs/HelloWorld.pdf">Hello World.pdf</a></li>
+                <li><a href="PDFs/HelloWorld_V2.pdf">Innovative Hello World.pdf</a></li>
+                <li><a href="PDFs/Play_With_CSS.pdf">Play with CSS.pdf</a></li>
+                <li><a href="PDFs/Mimic_a_Print_Advertisement.pdf">Mimic an ad.pdf</a></li>
+                <li><a href="PDFs/PHP_1.pdf">PHP 1.pdf</a></li>
+                <li><a href="PDFs/JavaScript_Play.pdf">JavaScript Play.pdf</a></li>
+            </ul>
+        </div>
+    </body>
+</html>
